add4035e7fc6398e82ad7234f4ae267affd7b7a0
DuryabKhan <khanduryab1@gmail.com>
2024-09-07 15:27:06 +0500

    day 2, done

--- Array/FindPairInArray.py
+++ Array/FindPairInArray.py
@@ -1,26 +1,17 @@
-# Example
-# Input Array
-# nums = [8, 7, 2, 5, 3, 1]
-# target = 10
-# Output 
-# pair found at (8, 2)
-# paior found at (7, 3)
 
 def findAllPairs(nums, target):
     pairs = []  # List to store all pairs
-
     for i in range(len(nums) - 1):
         for j in range(i + 1, len(nums)):
             if nums[i] + nums[j] == target:
                 pairs.append((nums[i], nums[j]))
-
     if pairs:
         for pair in pairs:
             print("pair found:", pair)
     else:
         print("pair not found")
 
+        
 nums = [8, 7, 2, 5, 3, 1]
 target = 10
-
 findAllPairs(nums, target)
